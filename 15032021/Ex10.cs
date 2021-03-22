f2852cb6d892b17492be169cca1d264fec504f33
Lucas T S Nogueira <62080330+lucasnogueira10@users.noreply.github.com>
2021-03-22 20:00:51 -0300

    Add files via upload

---
+++ 15032021/Ex10.cs
@@ -0,0 +1,22 @@
+﻿using Model.Ex10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class Ex10
+    {
+        static void Main(string[] args)
+        {
+            Estatisticas estatisticas = new Estatisticas();
+
+            estatisticas.ReadData();
+            estatisticas.DisplayStatistics();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 15032021/Ex14.cs
@@ -0,0 +1,21 @@
+﻿using Model.Ex14;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class Ex14
+    {
+        static void Main(string[] args)
+        {
+            Calculos calculos = new Calculos();
+
+            calculos.CalculateAverage();
+            
+            Console.ReadKey();
+        }
+    }
+}
