78d2fef677c54ed853ac468fc4d5088e43f4b643
Lucas T S Nogueira <62080330+lucasnogueira10@users.noreply.github.com>
2021-03-29 18:03:54 -0300

    Commit

---
+++ 22032021/Dal/IFerramentaDB.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public interface IFerramentaDB
+    {
+        bool Insert(Ferramenta ferramenta);
+        bool Update(Ferramenta ferramenta);
+        bool Delete(int id);
+        Ferramenta SelectById(int id);
+        List<Ferramenta> GetAll();
+    }
+}
